8d2d57f4abd95df9a84716785848b2fe0f93bc61
Richard Bain <rbain@iowatelecom.net>
2020-06-09 19:00:43 -0500

    refreshed source code

--- src/BlazorWasmSpiderTouch/Models/PlayingCardModel.cs
+++ src/BlazorWasmSpiderTouch/Models/PlayingCardModel.cs
@@ -1,4 +1,7 @@
-﻿namespace BlazorWasmSpiderTouch.Models
+﻿using System;
+using System.Text;
+
+namespace BlazorWasmSpiderTouch.Models
 {
     public class PlayingCardModel
     {
@@ -7,7 +10,14 @@
         public int Value { get; set; }
         public Suit Suit { get; set; }
         public string Description { get { return IsFaceDown ? "Back of Card" : CardIds.Substring(Value-1, 1) + " " + Suit.ToString(); } }
-        public string ImageUrl { get { return IsFaceDown ? "Images/2B.svg" : "Images/" + CardIds.Substring(Value - 1, 1) + this.Suit.ToString().Substring(0,1) + ".svg"; } }
+        public string ImageUrl
+        {
+            get
+            {
+                string key = IsFaceDown ? "2B" : CardIds.Substring(Value - 1, 1) + this.Suit.ToString().Substring(0, 1);
+                return "data:image/svg+xml;base64," + Convert.ToBase64String(Encoding.UTF8.GetBytes(PlayingCardDeck.SvgDefinitions[key]));
+            }
+        }
         public int TableauId { get; set; }
         public int StockId { get; set; }
         public int FoundationId { get; set; }
